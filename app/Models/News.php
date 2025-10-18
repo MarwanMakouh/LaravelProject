7d06ffd5da7ca21cb244e4e71bee529bd2512796
MarwanMakouh <marwan.makouh@student.ehb.be>
2025-10-18 16:01:51 +0200

    routes en migration toeveogen

---
+++ app/Models/News.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    protected $table = 'news';
+
+    protected $fillable = [
+        'title',
+        'content',
+        'image_url',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+}
